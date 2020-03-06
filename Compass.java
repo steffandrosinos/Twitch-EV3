5da424073d26da01c427a85f68c28c2c2e565989
Steffan Jones <SteffanDrosinos@outlook.com>
2020-03-06 22:17:06 +0000

    Robot update

--- Compass.java
+++ Compass.java
@@ -1,40 +1,70 @@
+package Main;
 
 public class Compass {
 
-  public int[] position = new int[2];
-  public int bearing;
-
-  public Compass(int pos_x, int pos_y, int bearing) {
-    this.position[0] = pos_y;
-    this.position[1] = pos_x;
-    this.bearing = bearing;
-  }
+	public int[][] Map = new int[6][6]; // Map[y][x]
+	public int[] position = new int[2];
+	public int bearing;
+  
+	public Compass(int pos_x, int pos_y, int bearing) {
+		this.position[0] = pos_y;
+		this.position[1] = pos_x;
+		this.bearing = bearing;
+		
+		/*
+		 * Map Types:
+		 *    0 -> Empty space
+		 *    1 -> Block
+		 *    2 -> Green
+		 *    3 -> Yellow
+		 *    4 -> Cyan
+		 *    5 -> Burgundy
+		 */
+		 // Set all of map to empty spaces
+		 for(int y=0; y<Map.length; y++) {
+		   for(int x=0; x<Map.length; x++) {
+		     Map[y][x] = 0;
+		   }
+		 }
+		 // Blocks
+		 Map[1][1] = 1;
+		 Map[0][2] = 1;
+		 Map[1][3] = 1;
+		 Map[3][2] = 1;
+		 Map[4][1] = 1;
+		 // Colours
+		 Map[3][1] = 2;
+		 Map[5][5] = 2;
+		 Map[2][0] = 3;
+		 Map[4][0] = 4;
+		 Map[3][4] = 5;
+	}
 
   //Getters
-  public int[] getPosition() {
-    return this.position;
-  }
+	public int[] getPosition() {
+		return this.position;
+	}
 
-  public boolean checkMove(String dir) {
-    int dir_deg = getDirectionAsDeg(dir);
-    int bearing_after_move = moveBearing(dir_deg);
-    return checkPossible(bearing_after_move);
-  }
+	public boolean checkMove(String dir) {
+		int bear = getDirectionAsDeg(dir);
+		System.out.println("nBear: " + bear);
+		return checkPossible(moveBearing(bear));
+	}
 
-  //function that returns degrees given direction to move, relative degree move
-  public int getDirectionAsDeg(String dir) {
-    if(dir.equals("Forward")) {
-      return 0;
-    } else if(dir.equals("Right")) {
-      return 90;
-    } else if(dir.equals("Left")) {
-      return -90;
-    } else if(dir.equals("Backwards")) {
-      return 180;
-    } else {
-      return -1;
-    }
-  }
+	//function that returns degrees given direction to move, relative degree move
+	public int getDirectionAsDeg(String dir) {
+		if(dir.equals("Forward")) {
+			return 0;
+	    } else if(dir.equals("Right")) {
+	    	return 90;
+	    } else if(dir.equals("Left")) {
+	    	return -90;
+	    } else if(dir.equals("Backwards")) {
+	    	return 180;
+	    } else {
+	    	return -1;
+	    }
+	}
 
   public int moveBearing(int deg) {
     int bearing_temp = this.bearing;
@@ -57,28 +87,27 @@ public class Compass {
     current_pos = this.position;
     int [] new_position = movePosition(current_pos, bearing_after);
     boolean possible = false;
-    if(new_position[0] >= 0 && new_position[0] <= 6) {
-      possible = true;
-    }
-    if(new_position[1] >= 0 && new_position[1] <= 6) {
-      possible = true;
+    System.out.println("nY: " + new_position[0] + " nX: " + new_position[1]);
+    if(Map[new_position[0]][new_position[1]] != 1) {
+	    if(new_position[0] >= 0 && new_position[0] <= 6) {
+	      possible = true;
+	    }
+	    if(new_position[1] >= 0 && new_position[1] <= 6) {
+	      possible = true;
+	    }
     }
     return possible;
   }
 
   public int[] movePosition(int[] current_pos, int dir_deg) {
-    if(dir_deg == 0) {
-      //Change Y+1
-      current_pos[0]++;
+    if(dir_deg == 0) { 
+      current_pos[0]++; //Change Y+1
     } else if(dir_deg == 90) {
-      //Change X+1
-      current_pos[1]++;
+      current_pos[1]++; //Change X+1
     } else if(dir_deg == 180) {
-      //Change Y-1
-      current_pos[0]--;
+      current_pos[0]--; //Change Y-1
     } else if(dir_deg == 270) {
-      //Change X-1
-      current_pos[1]--;
+      current_pos[1]--; //Change X-1
     }
     return current_pos;
   }
@@ -102,6 +131,7 @@ public class Compass {
     int dir_deg = getDirectionAsDeg(dir);
     this.bearing = moveBearing(dir_deg);
     this.position = movePosition(this.position, dir_deg);
+    System.out.println("X: " + this.position[0] + " Y: " + this.position[1]);
   }
 
 }
--- Robot.java
+++ Robot.java
@@ -31,18 +31,19 @@ public class Robot {
 	private float [] sample;
 
 	public int FORWARD_SPEED = 25;
+	public double ROTATE_FIX = 1.01;
 
 	public Robot(int PORT, int starting_x, int starting_y, int starting_bearing) {
 		this.PORT = PORT;
 
-		this.leftWheel = WheeledChassis.modelWheel(Motor.B, 3.6).offset(-4.5);
-		this.rightWheel = WheeledChassis.modelWheel(Motor.D, 3.6).offset(4.5);
+		this.leftWheel = WheeledChassis.modelWheel(Motor.B, 3.5).offset(-4.5);
+		this.rightWheel = WheeledChassis.modelWheel(Motor.D, 3.5).offset(4.5);
 		this.myChassis = new WheeledChassis( new Wheel[]{this.leftWheel, this.rightWheel}, WheeledChassis.TYPE_DIFFERENTIAL);
 		this.Pilot = new MovePilot(this.myChassis);
-		this.Pilot.setLinearAcceleration(10);
-		this.Pilot.setAngularAcceleration(60);
+		this.Pilot.setLinearAcceleration(5);
+		this.Pilot.setAngularAcceleration(30);
 		this.Pilot.setLinearSpeed(this.FORWARD_SPEED);
-		this.Pilot.setAngularSpeed(90);
+		this.Pilot.setAngularSpeed(20);
 
 		this.gyroSensor = new EV3GyroSensor(this.myEV3.getPort("S4"));
 		this.spr = this.gyroSensor.getAngleMode();
@@ -50,15 +51,15 @@ public class Robot {
 
 		this.compass = new Compass(starting_x, starting_y, starting_bearing);
 		this.server = new Server(this.PORT);
-		//this.server.connect();
+		this.server.connect();
 	}
 
 	public void start() {
-		this.Pilot.rotate(90);
-
+		System.out.println("Started");
 		while(true) {
 			if(!this.moving) {
 				String direction = server.getInput();
+				System.out.println("dir: " + direction);
 				move(direction);
 			}
 		}
@@ -82,86 +83,27 @@ public class Robot {
 				moveBackwards();
 				compass.compassUpdate(direction);
 			}
-		} else {
-			//Move not possible
 		}
 		this.moving = false;
 	}
 
 	public void moveForward() {
-		this.Pilot.travel(25);
+		this.Pilot.travel(21.2);
 	}
 
 	public void moveLeft() {
-		Rotate(-90);
-		this.Pilot.travel(25);
+		this.Pilot.rotate(-90);
+		moveForward();
 	}
 
 	public void moveRight() {
-		Rotate(90);
-		this.Pilot.travel(25);
+		this.Pilot.rotate(90);
+		moveForward();
 	}
 
 	public void moveBackwards() {
-		Rotate(180);
-		this.Pilot.travel(25);
-	}
-
-	// Function that rotates the robot given input degree
-	public void Rotate(int deg) {
-		this.Pilot.rotate(deg);
-
-		double GyroReading = getGyroSample();
-		double fix;
-		if(deg==90||deg==-90) {
-			fix = CheckRotation(GyroReading);
-		}else {
-			fix = CheckRotation180(GyroReading);
-		}
-
-		while(fix != 0) {
-			this.Pilot.rotate(fix);
-			//Delay.msDelay(100);
-			GyroReading = getGyroSample();
-			if(deg == 90 || deg == -90) {
-				fix = CheckRotation(GyroReading);
-			} else {
-				fix = CheckRotation180(GyroReading);
-			}
-		}
-
-		updateBearing(deg);
-		this.gyroSensor.reset();
-	}
-
-	public void tinyRotate(int x) {
-		this.Pilot.rotate(x);
-		this.gyroSensor.reset();
-	}
-
-	// Rotation checking 90deg
-	public static double CheckRotation(double x) {
-		double val = 0;
-		if(x > 0) {
-			val = 90 - x;
-		} else {
-			val = -90 - x;
-		}
-		if (val > 0.1 || val < -0.1) {
-			return val;
-		} else return 0;
-	}
-
-	public static double CheckRotation180(double x) {
-		double val = 0;
-		if(x > 0) {
-			val = 180 - x;
-		} else {
-			val = -180 - x;
-		}
-		if (val > 0.1 || val < -0.1) {
-			return val;
-		} else return 0;
+		this.Pilot.rotate(180);
+		moveForward();
 	}
 
 	public double getGyroSample() {
